aec42e13f06559754ed698b68cb8aaa9517d97c6
ms_sharpius <85020609+kirill-blip@users.noreply.github.com>
2022-02-02 21:47:07 +0400

    Fix player movement bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour, IDamageable
 {
+    #region
+
     public GameObject bulletPrefab;
     public Transform bulletPosition;
     public Transform movePoint;
     public float speed = 5;
     public float radius;
     public float distance = 0.2f;
-    public float dis = .8f;
     public LayerMask mask;
 
     public LayerMask whatStopsMovement;
@@ -21,55 +22,75 @@ public class PlayerController : MonoBehaviour, IDamageable
 
     public event EventHandler<GameObject> playerDestroyed;
 
+    [SerializeField]
+    private float dis = .8f;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
         movePoint.parent = null;
     }
-
     // Update is called once per frame
     void Update()
     {
+        // Movement
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
 
-
-        if (Vector3.Distance(transform.position, movePoint.position) <= distance)
+        if (Vector3.Distance(transform.position, movePoint.position) == distance)
         {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), radius, whatStopsMovement))
+                if (Input.GetAxisRaw("Horizontal") < 0)
                 {
-
+                    dis = Mathf.Abs(dis);
+                }
+                if (Input.GetAxisRaw("Horizontal") > 0)
+                {
+                    dis = -Mathf.Abs(dis);
+                }
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal") + dis, 0f, 0f), radius, whatStopsMovement))
+                {
                     if (Input.GetAxisRaw("Horizontal") < 0)
                     {
-                        movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal") + dis, 0f, 0f);
                         transform.eulerAngles = new Vector3(0f, 0f, 90f);
                     }
                     if (Input.GetAxisRaw("Horizontal") > 0)
                     {
-                        movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal") - dis, 0f, 0f);
                         transform.eulerAngles = new Vector3(0f, 0f, -90f);
                     }
+
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal") + dis, 0f, 0f);
                 }
             }
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), radius, whatStopsMovement))
+                if (Input.GetAxisRaw("Vertical") < 0)
+                {
+                    dis = Mathf.Abs(dis);
+                }
+                if (Input.GetAxisRaw("Vertical") > 0)
+                {
+                    dis = -Mathf.Abs(dis);
+                }
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical") + dis, 0f), radius, whatStopsMovement))
                 {
                     if (Input.GetAxisRaw("Vertical") < 0f)
                     {
-                        movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical") + dis, 0f);
                         transform.eulerAngles = new Vector3(0f, 0f, 180f);
                     }
                     else if (Input.GetAxisRaw("Vertical") > 0f)
                     {
-                        movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical") - dis, 0f);
                         transform.eulerAngles = Vector3.zero;
                     }
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical") + dis, 0f);
                 }
             }
         }
 
+
+
+        // Shooting
         currentShootingTime += Time.deltaTime;
 
         if (Input.GetButton("Jump") && currentShootingTime >= maxShootingTime)
@@ -78,7 +99,6 @@ public class PlayerController : MonoBehaviour, IDamageable
             currentShootingTime = 0;
         }
     }
-
     void Shoot()
     {
         GameObject go = Instantiate(bulletPrefab, bulletPosition.position, bulletPosition.rotation);
